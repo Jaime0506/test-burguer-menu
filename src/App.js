86d9bbb5f9e1f85702ab55e00225d61cad302283
Jaime Mejia <jaimemejia1003@gmail.com>
2022-03-31 21:19:38 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { bubble as Menu } from 'react-burger-menu'
 
-function App() {
+import './App.css'
+export default function App() {
+
+  const [windowHeight, setWindowHeight] = useState({})
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    const height =  window.innerHeight
+
+    setWindowHeight({
+      height
+    })
+  }, [])
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Menu 
+        right
+        customBurgerIcon={false}
+        isOpen={isOpen}
+        onOpen={ toggle }
+        onClose={ toggle }
+      > 
+        <div className='items-menu'>
+          <h1>Hola</h1>
+          <h1>Mi Gente</h1>
+          <h1>Linda</h1>
+          <h1>De Jaime</h1>
+        </div>
+        
+      </Menu>
+
+      <div className='app' style={windowHeight}>
+        <button onClick={ toggle }>Abrir Menu</button>
+      </div>
+    </>
+    
+  )
 }
 
-export default App;
